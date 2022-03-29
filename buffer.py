3356a181917a8847d440556744b38961bd34f201
Andy Stewart <lazycat.manatee@gmail.com>
2022-03-29 15:06:26 +0800

    Port to PyQt6

--- buffer.py
+++ buffer.py
@@ -19,8 +19,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt5.QtCore import QUrl
-from PyQt5.QtGui import QColor
+from PyQt6.QtCore import QUrl
+from PyQt6.QtGui import QColor
 from core.webengine import BrowserBuffer
 from core.utils import interactive, message_to_emacs
 from pathlib import Path
